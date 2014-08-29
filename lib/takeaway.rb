3f769455f232a68f2824408afa251474f32883e7
Henry Stanley <henry@henrystanley.com>
2014-08-29 15:15:45 +0100

    added tests and methods for listing takeaway menu with prices

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,3 +1,7 @@
 class Takeaway
 
+	def self.list_dishes
+		dishes = {"Fiorentina" => 8, "Hawaiian" => 9, "Margherita" => 6, "Pepperoni" => 8, "garlic bread" => 3}
+	end
+
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,5 +1,14 @@
 require 'takeaway'
 
 describe Takeaway do
+	
+	it 'should list all dishes' do	
+		expect(Takeaway.list_dishes).to include("Fiorentina", "Hawaiian", "Margherita", "Pepperoni", "garlic bread")
+	end
+
+	it 'all dishes should have a price' do
+		takeaway_prices = Takeaway.list_dishes.values
+		expect(takeaway_prices).to include(8, 9, 6, 8, 3)
+	end
 
 end
