1144cf53c3f03796cc97d859a216ca1cc63ee222
Henry Stanley <henry@henrystanley.com>
2014-08-29 18:34:37 +0100

    changed price list to a constant, added calculate total method (not yet implemented)

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -3,12 +3,42 @@ require 'takeaway'
 describe Takeaway do
 	
 	it 'should list all dishes' do	
-		expect(Takeaway.list_dishes).to include("Fiorentina", "Hawaiian", "Margherita", "Pepperoni", "garlic bread")
+		expect(Takeaway::PRICE_LIST).to include("Fiorentina", "Hawaiian", "Margherita", "Pepperoni", "dip")
 	end
 
 	it 'all dishes should have a price' do
-		takeaway_prices = Takeaway.list_dishes.values
-		expect(takeaway_prices).to include(8, 9, 6, 8, 3)
+		expect(Takeaway::PRICE_LIST.values).to include(8, 9, 6, 8, 3)
 	end
+
+	it 'should accept an order as a string' do
+		expect(Takeaway.place_order("1 Fiorentina, 2 Margherita, 1 dip, £23")).to eq "Order placed!"
+	end
+
+	it 'should convert the order string into an array of items' do
+		expect(Takeaway.order_split("1 Fiorentina, 2 Margherita, 1 dip, £23")).to eq(["1 Fiorentina", "2 Margherita", "1 dip"])
+	end
+
+	it 'should remove the leading £ sign from the order total provided by the user' do
+		Takeaway.place_order("1 Fiorentina, 2 Margherita, 1 dip, £23")
+		expect(Takeaway.user_total).to eq 23
+	end
+
+	xit 'should take each order line and split it into quantity and item' do
+		Takeaway.place_order("1 Fiorentina, 2 Margherita, 1 dip, £23")
+		expect(Takeaway.order_item_split).to eq()
+	end
+
+	xit 'should calculate the order total' do
+		Takeaway.place_order("1 Fiorentina, 2 Margherita, 1 dip, £26")
+		expect(Takeaway.calculate_total).to eq 23
+	end
+
+	xit 'should not accept an order with an invalid total' do
+		expect{ Takeaway.place_order("1 Fiorentina, 2 Margherita, 1 dip, £58") }.to raise_error
+	end
+
+	it 'should send a text saying that your order will be delivered in one hour' do
+		end
+
 
 end
